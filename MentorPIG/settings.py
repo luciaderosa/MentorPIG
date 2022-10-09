ef45cf23206e24ee9fa834d661414fb236f6803a
Rodrigo Lascano <rodrigo.lascano@gmail.com>
2022-10-08 22:57:52 -0300

    Se seteo db, se agrego modelo Event, se cargaron datos de prueba, el template events.html muestra eventos en un for

--- MentorPIG/settings.py
+++ MentorPIG/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "MentorPIG.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mentor",
+        "USER": "mentoruser",
+        "PASSWORD": "mentorpassword",
+        "HOST": "localhost",
+        "PORT": ""
     }
 }
 
--- mentor/views.py
+++ mentor/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
+from mentor.models import Event
+
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'mentor/index.html')
+
 
 def contact(request):
     return render(request, 'mentor/contact.html')  
 
+
 def about(request): 
     return render(request, 'mentor/about.html')
+
 
 def courses(request):
     return render(request, 'mentor/courses.html')   
 
+
 def trainers(request):
     return render(request, 'mentor/trainers.html')   
 
+
 def events(request):
-    return render(request, 'mentor/events.html')     
+    events = Event.objects.all()
+    return render(request, 'mentor/events.html', {'events': events})     
+
 
 def course_detail(request):
     return render(request, 'mentor/course-details.html')           
