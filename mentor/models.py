a9f6060c7c415c7dc7082a4854d54b1be46e1cb4
Rodrigo Lascano <rodrigo.lascano@gmail.com>
2022-10-10 20:57:33 -0300

    Se mejoro filtro date en events.html

--- mentor/models.py
+++ mentor/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=100)                
-    text = models.CharField(max_length=255)             
-    img_path = models.CharField(max_length=100)   
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=255)
+    img_path = models.CharField(max_length=100)
     date = models.DateTimeField()
 
     def __str__(self):
