1f7319e010fe1c4843e5cd1f9030ff66ceca4255
luciaderosa <luciaderosa@hotmail.com>
2022-10-13 17:17:42 -0300

    Models Trainers-Testimonial

--- mentordata/models.py
+++ mentordata/models.py
@@ -1,38 +1,61 @@
-from pickletools import ArgumentDescriptor
 from django.db import models
 
-# Create your models here.
-
-#anterior a modificar
-# class Event(models.Model):
-#     title = models.CharField(max_length=100)                
-#     text = models.CharField(max_length=255)             
-#     img_path = models.CharField(max_length=100)   
-#     date = models.DateTimeField()
-
-#     def __str__(self):
-#         return self.title
 
 class Event(models.Model):
     # Campos y atributos
     # max_length= 20 — Establece que la longitud máxima del valor de este campo es 20 caracteres.
     # help_text="texto..." — proporciona una etiqueta de texto para mostrar que ayuda.
 
-    title = models.CharField(max_length=100, help_text="Título del evento")                
-    text =models.CharField(max_length=255,help_text="Resumen del evento")
-    date=models.DateTimeField(help_text="Fecha y hora del evento")
-    image = models.ImageField(upload_to ="projects")
-    created=models.DateTimeField(auto_now_add = True)
-    updated=models.DateTimeField(auto_now = True)  
+    title = models.CharField(max_length=100, help_text="Título del evento")
+        # Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
+    text = models.TextField(help_text="Resumen del evento")
+    date = models.DateTimeField(help_text="Fecha y hora del evento")
+    image = models.ImageField(upload_to="projects")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     # Metadata
     class Meta:
-    # para que el evento más nuevo quedo primero    
+        # para que el evento más nuevo quedo primero
         ordering = ["-date"]
 
     # Métodos
-   
+
     def __str__(self):
-        #Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
+        # Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
         return self.title
-    
+
+
+class Trainer(models.Model):
+
+    first_name = models.CharField(max_length=100, help_text="Nombre")
+    last_name = models.CharField(max_length=100, help_text="Apellido")
+    title = models.CharField(max_length=100, help_text="Título")
+    brief = models.TextField(help_text="Resumen curricular")
+    photo = models.ImageField(upload_to="projects", help_text="Foto")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta ():
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        # Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
+        return self.last_name + ", " + self.first_name
+
+
+class Testimonial(models.Model):
+
+    name = models.CharField(max_length=100, help_text="Nombre")
+    title = models.CharField(max_length=100, help_text="Título")
+    text = models.TextField(help_text="Texto del testimonio")
+    image = models.ImageField(upload_to="projects")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # el orden de ingreso? la fecha de creación? del más viejo al más nuevo?
+        ordering = ["created"]
+
+    def __str__(self):
+        return self.name
